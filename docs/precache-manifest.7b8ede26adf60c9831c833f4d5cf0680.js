272aa1606131d4f684f518a82f11936dbbfcb390
Dylan Rolfe <dylan.rolfe@health.gov.au>
2020-09-07 09:41:49 +1000

    Build

---
+++ docs/precache-manifest.7b8ede26adf60c9831c833f4d5cf0680.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3542b21f65ce8ee43acacd8a81e06b40",
+    "url": "/gm-tools/index.html"
+  },
+  {
+    "revision": "1107447ffebe5b6a629c",
+    "url": "/gm-tools/static/css/2.2d2334db.chunk.css"
+  },
+  {
+    "revision": "f8b4c2c4a607104de5ab",
+    "url": "/gm-tools/static/css/main.df891d08.chunk.css"
+  },
+  {
+    "revision": "1107447ffebe5b6a629c",
+    "url": "/gm-tools/static/js/2.26d40848.chunk.js"
+  },
+  {
+    "revision": "45a2c04b17b5fa46b6c4dc216cdeeda5",
+    "url": "/gm-tools/static/js/2.26d40848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b4c2c4a607104de5ab",
+    "url": "/gm-tools/static/js/main.40efb189.chunk.js"
+  },
+  {
+    "revision": "75a71eba3f490166093f",
+    "url": "/gm-tools/static/js/runtime-main.21b63df0.js"
+  },
+  {
+    "revision": "2fa6748c87e1ed39dda9fa949dd77f84",
+    "url": "/gm-tools/static/media/BaskervaldADFStd.ttf.2fa6748c.svg"
+  },
+  {
+    "revision": "3f093510edb75bcc4eaa745b91c1fc93",
+    "url": "/gm-tools/static/media/BaskervaldADFStd.ttf.3f093510.eot"
+  },
+  {
+    "revision": "57d819d15ef0081ac13acf1800d80f06",
+    "url": "/gm-tools/static/media/BaskervaldADFStd.ttf.57d819d1.woff"
+  }
+]);
